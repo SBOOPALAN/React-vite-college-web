20ae1f41ae9960c32cdc284a414496ee91a7d8e8
SBOOPALAN <bboobalan379@gmail.com>
2024-06-28 14:25:21 -0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react()],
-  base: 'react-vite-clg-web',
-});
+  plugins: [react(), ghPages()],
+  base: 'react-vite-clg-web' 
+})
